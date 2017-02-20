7897afdf495a4bb5374765b13fd3a9caa3478a8e
Xavier Agostini <xavier.agostini@mail.mcgill.ca>
2017-02-20 16:39:10 -0500

    first commit

---
+++ src/components/Todo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>This is a test todo: {{message}}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['message']
+  }
+</script>
+
+<style scoped>
+  div {
+    border-bottom: 2px solid black;
+  }
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Todos App</h1>
+    <div id="todoApp">
+      <add-todo></add-todo>
+      <todo v-for="todo in todos" v-bind:message="todo"></todo>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Todo from 'components/Todo'
+  import AddTodo from 'components/AddTodo'
+  export default {
+    data () {
+      return {
+        todos: ['test1', 'test2', 'test3']
+      }
+    },
+    components: {
+      Todo,
+      AddTodo
+    }
+  }
+</script>
+
+<style scoped>
+  #todoApp {
+    width: 500px;
+    margin: 0 auto;
+    background-color: #eee;
+  }
+</style>
