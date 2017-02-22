bf2a0090820e59e54b926e0fe33c6a815be82ac3
Xavier Agostini <xavier.agostini@mail.mcgill.ca>
2017-02-21 22:38:14 -0500

    add showCompleted functionality

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -3,15 +3,15 @@
     <h1>Todos App</h1>
     <div id="todoApp">
       <input type="text" v-model="newTodoText" v-on:keyup.enter="addNewTodo" placeholder="Add a todo" class="addBox" />
-      <button v-on:click="showFunc" v-if="showAll === true">Show Completed</button>
-      <button v-on:click="showFunc" v-else>Show All</button>
+      <input type="checkbox" name="showCompleted" v-on:change="toggleCompleted">
+      <label for="showCompleted">Show Completed</label>
       <button v-on:click="clearTodos">Clear All</button>
       <div v-show="todos.length == 0">
         No Items To Show
       </div>
-      <div v-for="(todo, index) in todos" class="todo" v-on:click="editTodo(index)">
+      <div v-for="(todo, index) in todos" v-if="!todo.isComplete || !showCompleted" v-on:click="editTodo(index)" class="todo">
         <div>
-          <input type="checkbox" name="isCompleted" v-on:change="completeTodo(index)" class="todoCheck"/>
+          <input type="checkbox" name="isCompleted" v-on:change="completeTodo(index)" class="todoCheck" v-model="todo.showCompleted"/>
           <label v-bind:class="{completed: todo.isCompleted}">{{todo.text}}</label>
         </div>
         <div>
@@ -23,7 +23,7 @@
         </div>
       </div>
       <div>
-        {{todos.length}} items left
+        {{todos | numCompleted(todos)}} items completed | {{todos.length}} items left
       </div>
     </div>
   </div>
@@ -35,14 +35,14 @@
       return {
         newTodoText: '',
         todos: [],
-        showAll: true
+        showCompleted: true
       }
     },
     methods: {
       addNewTodo: function () {
         this.todos.push({
           'text': this.newTodoText,
-          'isCompleted': false,
+          'isComplete': false,
           'dateCreated': new Date()
         })
         this.newTodoText = ''
@@ -51,16 +51,25 @@
         this.todos.splice(index, 1)
       },
       completeTodo: function (index) {
-        this.todos[index].isCompleted = !this.todos[index].isCompleted
+        this.todos[index].isComplete = !this.todos[index].isComplete
       },
       clearTodos: function () {
         this.todos = []
       },
-      showFunc: function () {
-        this.showAll = !this.showAll
+      toggleCompleted: function () {
+        this.showCompleted = !this.showCompleted
       },
       editTodo: function (i) {
         this.todos[i].text += ' test'
+      }
+    },
+    filters: {
+      numCompleted: function (todos) {
+        var count = 0
+        for (var i = 0; i < todos.length; i++) {
+          if (todos[i].isComplete) count++
+        }
+        return count
       }
     }
   }
@@ -78,7 +87,7 @@
   #todoApp {
     width: 500px;
     margin: 0 auto;
-    background-color: #eee;
+    background-color: #fff;
   }
   .todo {
     border-bottom: 1px solid black;
