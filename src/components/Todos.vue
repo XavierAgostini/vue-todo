b682e7838ca21d223f9f864c94aaf71935873444
Xavier Agostini <xavier.agostini@laserfiche.com>
2017-02-24 14:08:04 -0500

    improved design, reduced functionality

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,33 +1,28 @@
 <template>
   <div>
-    <h1>Todos App</h1>
+    <h1 class="centerText">Todos App</h1>
     <div id="todoApp">
-      <input type="text" v-model="newTodoText" v-on:keyup.enter="addNewTodo" placeholder="Add a todo" class="addBox"/>
-      <input type="checkbox" name="showCompleted" v-model="showCompleted">
-      <label for="showCompleted">Show Completed</label>
-      <button v-on:click="clearTodos" class="btn btn-outline-danger">Clear All</button>
-      <hr>
-      <div v-show="todos.length == 0">
-        No Items To Show
+      <div class="addBox">
+        <input type="text" v-model="newTodoText" v-on:keyup.enter="addNewTodo" placeholder="Add a todo"/>
       </div>
       <div v-for="(todo, index) in todos" v-if="!todo.isComplete || showCompleted" v-on:click="editTodo(index)" class="todo">
-        <div>
-          <input type="checkbox" name="isCompleted" v-on:change="completeTodo(index)" class="todoCheck" v-model="todo.showCompleted"/>
-          <label v-bind:class="{completed: todo.isCompleted}" v-on:click="editTodo(index)" class="todoText" v-if="editIndex != index">{{todo.text}}</label>
-          <input type="text" v-on:keyup.enter="updateTodo(index)" v-model="todo.text" class="editBox active" v-else/>
-        </div>
-        <div>
-            
-        </div>
-        <div class="todoInfo">
-          <span v-if="todo.lastModified === 'Never'">Created: {{todo.dateCreated | formatDate}}</span>
-          <span v-else>Modified: {{todo.lastModified | formatDate}}</span>
-          <button v-on:click="deleteTodo(index)" class="btn btn-danger">X</button>      
-        </div>
+        <input type="checkbox" name="isCompleted" v-on:change="completeTodo(index)" class="todoCheck" v-model="todo.showCompleted"/>
+        <label v-bind:class="{completed: todo.isCompleted}" v-on:click="editTodo(index)" class="todoText">{{todo.text}}</label>
+        <button v-on:click="deleteTodo(index)" class="del-todo btn btn-danger">X</button>      
       </div>
       <div>
-        {{todos | numCompleted(todos)}} items completed | {{todos.length}} items left
+        <input type="checkbox" name="showCompleted" v-model="showCompleted">
+        <label for="showCompleted">Show Completed</label>
+        <div>
+           {{todos | numCompleted(todos)}}/{{todos.length}} Todos Completed
+        </div>
       </div>
+    </div>
+    <br>
+    <div class="instructionText">
+      <p>To Add a todo: Type your todo in the "Add todo" box then hit enter to add it</p>
+      <p>To Edit a todo: Double click to edit todo</p>
+      <p>To Delete a todo: Hover over todo and hit the close button
     </div>
   </div>
 </template>
@@ -59,9 +54,6 @@
       completeTodo: function (index) {
         this.todos[index].isComplete = !this.todos[index].isComplete
       },
-      clearTodos: function () {
-        this.todos = []
-      },
       editTodo: function (i) {
         this.editIndex = i
       },
@@ -89,17 +81,30 @@
 
 <style scoped>
   .addBox {
-    width: 496px; 
+     border: 2px solid #ddd;
+     height: 65px;
+  }
+  .addBox input {
+    width: 450px; 
     height: 40px;
+    margin-top: 10px;
     font-size: 25px;
-    text-align: center;
     padding: 0;
-    margin: 0;
+    margin: 10px 0 0 20px;
+    border: none;
+  }
+  .addBox input:focus {
+    outline: none;
+  }
+  .centerText {
+    text-align: center;
   }
   #todoApp {
     width: 500px;
     margin: 0 auto;
     background-color: #fff;
+    /*border: 2px solid #ddd;*/
+    box-shadow: 1px 1px 5px 0px rgba(0,0,0,0.5);
   }
   input[name="showCompleted"] {
     float: left;
@@ -110,18 +115,31 @@
     margin-left: 10px;
   }
   .todo {
-    border-bottom: 1px solid black;
+    position: relative;
+    border: 2px solid #ddd;
+    border-top: none;
     padding-top: 10px;
     height: 80px;
   }
   .todoCheck {
-    float: left;
-    margin-left: 10px;
+    position: absolute;
+    height: 40px;
+    width: 40px;
+    left: 10px;
+    top: 17px;
+    appearance: none;
+    outline: none;
   }
-  .todoInfo {
+  .todoCheck:after {
+    content: url('../../static/imgs/blank-circle.svg');
+  }
+  .todoCheck:checked:after {
+    content: url('../../static/imgs/checked-circle.svg');
+  }
+  .del-todo {
     display: none;
   }
-  .todo:hover .todoInfo{
+  .todo:hover .del-todo{
     display: inline-block;
   }
   .completed {
@@ -129,10 +147,9 @@
   }
   .todoText {
     font-size: 16px;
-    width: 94%;
-  }
-  .todoText:hover {
-    cursor: pointer;
+    width: 400px;
+    padding: 15px 60px 15px 15px;
+    margin-left: 45px;
   }
   .editBox {
     border:none;
@@ -143,11 +160,16 @@
     box-shadow: none;
     padding: 0;
     font-size: 16px;
-    text-align: center;
-    width: 94%;
+    width: 400px;
     margin-bottom: 0.5em;
   }
   .editBox:focus {
     outline: none;
+  }
+  .instructionText {
+    font-size: 12px;
+    width: 500px;
+    text-align: center;
+    margin: 0 auto;
   }
 </style>
