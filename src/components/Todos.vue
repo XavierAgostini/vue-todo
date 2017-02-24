c6b28b17d5de36ebadd8fea3cbe1e371d9c48f8d
Xavier Agostini <xavier.agostini@laserfiche.com>
2017-02-24 15:51:38 -0500

    add toggle add functionality and improve styling

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -3,20 +3,21 @@
     <h1 class="centerText">Todos App</h1>
     <div id="todoApp">
       <div class="addBox">
-        <input type="text" v-model="newTodoText" v-on:keyup.enter="addNewTodo" placeholder="Add a todo"/>
+        <input type="checkbox" v-model="toggleAll" class="toggleBtn" v-on:change="toggleTodos"/>
+        <input type="text" v-model="newTodoText" v-on:keyup.enter="addNewTodo" placeholder="Add a todo ..." v-focus class="addInput"/>
       </div>
       <div v-for="(todo, index) in todos" v-if="!todo.isComplete || showCompleted" v-on:click="editTodo(index)" class="todo">
-        <input type="checkbox" name="isCompleted" v-on:change="completeTodo(index)" class="todoCheck" v-model="todo.showCompleted"/>
-        <label v-bind:class="{completed: todo.isCompleted}" v-on:click="editTodo(index)" class="todoText"v-if="editIndex != index">{{todo.text}}</label>
-        <input type="text" v-model="todo.text" v-on:keyup.enter="updateTodo(index)" v-else class="editBox">
+        <input type="checkbox" name="isCompleted" v-model="todo.isComplete" v-on:change="completeTodo(index)" class="todoCheck" />
+        <label v-on:click="editTodo(index)" class="todoText" v-bind:class="{todoCompleted : todo.isComplete}" v-if="editIndex != index">{{todo.text}}</label>
+        <input type="text" v-model="todo.text" v-on:keyup.enter="updateTodo(index)" v-else class="editBox" v-focus>
         <button v-on:click="deleteTodo(index)" class="del-todo btn btn-danger">X</button>      
       </div>
-      <div>
+      <div v-show="todos.length > 0" class="todoInfo">
         <input type="checkbox" name="showCompleted" v-model="showCompleted">
         <label for="showCompleted">Show Completed</label>
-        <div>
+        <span>
            {{todos | numCompleted(todos)}}/{{todos.length}} Todos Completed
-        </div>
+        </span>
       </div>
     </div>
     <br>
@@ -32,10 +33,18 @@
   export default {
     data () {
       return {
+        toggleAll: false,
         newTodoText: '',
         todos: [],
         showCompleted: true,
         editIndex: -1
+      }
+    },
+    directives: {
+      focus: {
+        inserted: function (el) {
+          el.focus()
+        }
       }
     },
     methods: {
@@ -46,17 +55,23 @@
         })
         this.newTodoText = ''
       },
-      deleteTodo: function (index) {
-        this.todos.splice(index, 1)
+      deleteTodo: function (i) {
+        this.todos.splice(i, 1)
       },
-      completeTodo: function (index) {
-        this.todos[index].isComplete = !this.todos[index].isComplete
+      completeTodo: function (i) {
+        this.todos[i].isComplete = this.todos[i].isComplete || false
       },
       editTodo: function (i) {
         this.editIndex = i
       },
       updateTodo: function (i) {
         this.editIndex = -1
+      },
+      toggleTodos: function () {
+        console.log('toggle')
+        for (var i = 0; i < this.todos.length; i++) {
+          this.todos[i].isComplete = this.toggleAll
+        }
       }
     },
     filters: {
@@ -76,8 +91,8 @@
      border: 2px solid #ddd;
      height: 65px;
   }
-  .addBox input {
-    width: 450px; 
+  .addBox .addInput {
+    width: 440px; 
     height: 40px;
     margin-top: 10px;
     font-size: 25px;
@@ -85,8 +100,21 @@
     margin: 10px 0 0 20px;
     border: none;
   }
-  .addBox input:focus {
+  .addBox .addInput:focus {
     outline: none;
+  }
+  .toggleBtn {
+    margin-left: 10px;
+    appearance: none;
+    transform: rotate(90deg);
+    color: #ddd;
+  }
+  .toggleBtn:before {
+    content: ">";
+    font-size: 25px;
+  }
+  .toggleBtn:checked {
+    color: black;
   }
   .centerText {
     text-align: center;
@@ -113,6 +141,7 @@
     padding-top: 10px;
     height: 80px;
   }
+
   .todoCheck {
     position: absolute;
     height: 40px;
@@ -127,6 +156,10 @@
   }
   .todoCheck:checked:after {
     content: url('../../static/imgs/checked-circle.svg');
+  }
+  .todoCompleted {
+    text-decoration: line-through;
+    color: #ddd;
   }
   .del-todo {
     display: none;
@@ -143,6 +176,9 @@
     padding: 15px 60px 15px 15px;
     margin-left: 45px;
   }
+  .todoText:hover {
+    cursor: pointer;
+  }
   .editBox {
     border:none;
     background-image:none;
@@ -156,6 +192,19 @@
   }
   .editBox:focus {
     outline: none;
+  }
+  .todoInfo {
+    position: relative;
+    border: 2px solid #ddd;
+    border-top: none;
+    text-align: right;
+  }
+  .todoInfo span {
+    padding-right: 10px;
+  }
+  input[name="showCompleted"] {
+    margin-top: 5px;
+    margin-right: 5px;
   }
   .instructionText {
     font-size: 12px;
